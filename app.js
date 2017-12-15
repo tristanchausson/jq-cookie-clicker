5c01a49c21acf0610fe59aeb813c2e5a8e724faf
tristanchausson <tristanchausson@me.com>
2017-12-15 14:38:49 +0100

    add app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-	//votre code
+	var number = 0;
+	var mess = ["1","2","3","4","5"];
 
+		$('img').click(function(){
+			//console.log('click');
+				number ++;
+			//console.log(number);
+		$('.counter').html(number);
 
+			if (number%20 == 0){
+		
+				alert(mess[Math.floor(Math.random() * (4))]);
+			}
+		});
 });
